d16becce3fe3cb4683eaffc5082060b69aea0254
Drew Pihera <dpihera@gmail.com>
2024-05-13 10:26:37 -0400

    formatter run

--- etl_gphl_cre_alert.py
+++ etl_gphl_cre_alert.py
@@ -14,38 +14,39 @@ spark_ctx = SparkContext()
 glue_ctx = GlueContext(spark_ctx)
 logger = glue_ctx.get_logger()
 
-# TODO: 
+# TODO:
 #   - add error handling for the format of the document being incorrect
 #   - figure out how we want to name and namespace clean files (e.g. will we
 #     take the object key we're given, strip the extension and replace it with
 #     one for the new format, or will we do something else)
-#   - see what we can extract out of here to be useful for other ETLs. imagine 
-#     we'd have a few different things that could be made into a reusable 
+#   - see what we can extract out of here to be useful for other ETLs. imagine
+#     we'd have a few different things that could be made into a reusable
 #     package
 
 parameters = getResolvedOptions(
-    sys.argv, 
-    ["RAW_BUCKET_NAME", "ALERT_OBJ_KEY", "CLEAN_BUCKET_NAME",]
+    sys.argv,
+    [
+        "RAW_BUCKET_NAME",
+        "ALERT_OBJ_KEY",
+        "CLEAN_BUCKET_NAME",
+    ],
 )
 
 raw_bucket_name = parameters["RAW_BUCKET_NAME"]
 alert_obj_key = parameters["ALERT_OBJ_KEY"]
 clean_bucket_name = parameters["CLEAN_BUCKET_NAME"]
 
-# NOTE: for now we'll take the alert object key and change out the file 
-#       extension for the clean data (leaving all namespacing and such). this 
+# NOTE: for now we'll take the alert object key and change out the file
+#       extension for the clean data (leaving all namespacing and such). this
 #       will probably need to change
 clean_obj_key = alert_obj_key.replace(".docx", ".csv")
 
 # NOTE: May need some creds here
-s3_client = boto3.client('s3')
+s3_client = boto3.client("s3")
 
-# try to get the docx object from S3 and handle any error that would keep us 
+# try to get the docx object from S3 and handle any error that would keep us
 # from continuing.
-response = s3_client.get_object(
-    Bucket=raw_bucket_name, 
-    Key=alert_obj_key
-)
+response = s3_client.get_object(Bucket=raw_bucket_name, Key=alert_obj_key)
 
 status = response.get("ResponseMetadata", {}).get("HTTPStatusCode")
 
@@ -57,7 +58,7 @@ if status != 200:
 
     logger.error(err)
 
-    # NOTE: need to properly handle exception stuff here, and we probably want 
+    # NOTE: need to properly handle exception stuff here, and we probably want
     #       this going somewhere very visible (e.g. SNS topic or a perpetual log
     #       as someone will need to be made aware)
     raise Exception(err)
@@ -66,7 +67,7 @@ logger.info(f"Obtained object {alert_obj_key} from bucket {raw_bucket_name}.")
 
 # handle the document itself...
 
-# NOTE: this document is assumed to contain a single table that needs to be 
+# NOTE: this document is assumed to contain a single table that needs to be
 #       processed and nothing else. The file consists of:
 #       - a 2 column header row that contains a column (0 index) with the alert
 #         report date, which we need (rest of this row can be ignored)
@@ -79,33 +80,31 @@ data = [[cell.text for cell in row.cells] for row in table.rows]
 data = pd.DataFrame(data)
 
 # grab the alert report date and then drop that row
-date_received = pd.to_datetime(dparser.parse(data.iloc[0,0],fuzzy=True))
+date_received = pd.to_datetime(dparser.parse(data.iloc[0, 0], fuzzy=True))
 data.columns = data.loc[1]
-data = data[2:].reset_index(drop = True)
+data = data[2:].reset_index(drop=True)
 
 # now perform the ETL on the data rows
 # NOTE: Questions about the data:
-#           - Do we need to split this name to better enable queries later? 
-#           - will the name only ever be composed of first and last (i.e. no 
+#           - Do we need to split this name to better enable queries later?
+#           - will the name only ever be composed of first and last (i.e. no
 #             middle name handling)?
-#           - do we not want the lab id to carry over into clean data? same 
-#             with organism id (and anything else that doesn't carry over 
-#             currently). there's very little penalty (storage cost) for it 
-#             carrying over, and if it's not part of the AR log, we just don't 
-#             include it in the AR query, but we still have it if we end up 
+#           - do we not want the lab id to carry over into clean data? same
+#             with organism id (and anything else that doesn't carry over
+#             currently). there's very little penalty (storage cost) for it
+#             carrying over, and if it's not part of the AR log, we just don't
+#             include it in the AR query, but we still have it if we end up
 #             needing it for anything else
 interm = pd.DataFrame()
-interm["Mechanism (*Submitters Report)"] = data[
-    "Anti-Microbial Resistance RT-PCR"
-]
+interm["Mechanism (*Submitters Report)"] = data["Anti-Microbial Resistance RT-PCR"]
 interm["Organism"] = data["Organism ID"]
 interm["Date Received"] = date_received
 interm["Date Reported"] = date_received
 interm["Patient Name"] = data["Patient Name"]
-interm["DOB"] = pd.to_datetime(data["DOB"], errors = "coerce")
+interm["DOB"] = pd.to_datetime(data["DOB"], errors="coerce")
 interm["Source"] = data["Source"].str.capitalize()
 interm["Date of Collection"] = pd.to_datetime(
-    data["Date of Collection"], errors = "coerce"
+    data["Date of Collection"], errors="coerce"
 )
 interm["Testing Lab"] = "GPHL"
 interm["Facility of Origin"] = data["Received from"]
@@ -121,17 +120,17 @@ with io.StringIO() as csv_buff:
     status = response.get("ResponseMetadata", {}).get("HTTPStatusCode")
 
     if status != 200:
-	    err = (
-	        f"ERROR - Could not write transformed data object {clean_obj_key} "
-	        f"to bucket {clean_bucket_name}. ETL Cannot continue."
-	    )
-	
-	    logger.error(err)
-	
-	    # NOTE: need to properly handle exception stuff here, and we probably 
-	    #       want this going somewhere very visible (e.g. SNS topic or a 
-	    #       perpetual log as someone will need to be made aware)
-	    raise Exception(err)
+        err = (
+            f"ERROR - Could not write transformed data object {clean_obj_key} "
+            f"to bucket {clean_bucket_name}. ETL Cannot continue."
+        )
+
+        logger.error(err)
+
+        # NOTE: need to properly handle exception stuff here, and we probably
+        #       want this going somewhere very visible (e.g. SNS topic or a
+        #       perpetual log as someone will need to be made aware)
+        raise Exception(err)
 
     logger.info(
         f"Transformed {raw_bucket_name}/{alert_obj_key} and wrote result "
