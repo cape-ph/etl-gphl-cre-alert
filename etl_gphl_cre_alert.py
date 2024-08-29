4549ec1426f85c9099d5cea700dd689d94f21d62
Richardson, Peter <Peter.Richardson@gtri.gatech.edu>
2024-08-29 13:57:19 -0400

    Added state lab id column

--- etl_gphl_cre_alert.py
+++ etl_gphl_cre_alert.py
@@ -117,6 +117,7 @@ interim["Date of Collection"] = pd.to_datetime(
 )
 interim["Testing Lab"] = "GPHL"
 interim["Facility of Origin"] = data["Received from"]
+interim["State_Lab_ID"] = data["Lab ID"]
 
 # write out the transofrmed data
 with io.StringIO() as csv_buff:
